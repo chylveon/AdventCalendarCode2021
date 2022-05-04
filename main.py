f4d2dc475aef2f43755999cd45809e28c17b3565
James Stimac <jamesstimacdec@gmail.com>
2022-05-04 18:16:20 -0400

    day 1 part 2: created sum list of each item from a sliding window of 3 items.

--- main.py
+++ main.py
@@ -10,15 +10,32 @@ with open('input01.txt', 'r') as input_0:
 #testing list print(input_list[:7])
 
 #compare elements in input_list to increment increasing
-num_increasing = 0
-current_num = 0
-previous_num = 0
-for num in input_list:
-    current_num = num
-    if previous_num != 0:
-        if previous_num < current_num:
-            num_increasing += 1
-            previous_num = current_num
-    previous_num = current_num
+#num_increasing = 0
+#current_num = 0
+#previous_num = 0
+#for num in input_list:
+#    current_num = num
+#    if previous_num != 0:
+#        if previous_num < current_num:
+ #           num_increasing += 1
+ #           previous_num = current_num
+ #   previous_num = current_num
 
 # print(num_increasing) returns 1564 which is correct.
+
+# second question day 1
+list_len = len(input_list)
+i = 0
+j = 1
+k = 2
+sum_list = []
+while k < (list_len - 1):
+    while j < (list_len - 2):
+        while i < (list_len - 3):
+            sum_list.append(input_list[i] + input_list[j] + input_list[k])
+
+            i += 1
+        j += 1
+    k += 1
+
+print(sum_list)
