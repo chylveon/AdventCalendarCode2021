7e5316bb2e9a83398e43234683e597a62ec06b72
James Stimac <jamesstimacdec@gmail.com>
2022-05-04 17:55:52 -0400

    opened and read file input

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 # day 1
 # open file and create list with all input
 
+#read file:
+input_list = []
+with open('input01.txt', 'r') as input_0:
+    for num in input_0:
+        input_list.append(int(num))
+
+#testing list print(input_list[:7])
+
+
